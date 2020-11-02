7a02cead5b9d4922b0a7818a248fead4b7f22878
Zhuoyue Lyu <Zhuoyue.Lyu@ibm.com>
2020-11-01 19:53:42 -0500

    Runnable version! (TriOsc & Reverb & without pop sound!)

--- Assets/Scripts/Sonify.cs
+++ Assets/Scripts/Sonify.cs
@@ -12,15 +12,41 @@ public class Sonify : MonoBehaviour
     {
         myChuck1 = "my_chuck";
         Chuck.Manager.Initialize( mixerWithChuck, myChuck1 );
+        // Chuck.Manager.RunCode( myChuck1,
+        //     @"
+        //     fun void playSound( float val )
+        //     {
+        //         SinOsc foo => dac;
+        //         val => foo.freq;
+        //     }
+        //     external float val;
+        //     external Event play;
+
+        //     while( true )
+        //     {
+        //         play => now;
+        //         spork ~ playSound( val );
+        //     }
+
+        //     "
+		// );
+
         Chuck.Manager.RunCode( myChuck1,
             @"
+            TriOsc osc => ADSR env => NRev verb => dac;
+            0.3 => osc.gain;
+            0.01 => verb.mix;
+
+            env.set(10.0::ms, 60.0::ms, 0.0, 0::ms);
+
             fun void playSound( float val )
             {
-                SinOsc foo => dac;
-                val => foo.freq;
+                val => osc.freq;
+                env.keyOn();
             }
-            external float val;
-            external Event play;
+
+            global float val;
+            global Event play;
 
             while( true )
             {
@@ -30,6 +56,7 @@ public class Sonify : MonoBehaviour
 
             "
 		);
+
     }
 
     // Update is called once per frame
@@ -42,7 +69,7 @@ public class Sonify : MonoBehaviour
     {
 
         Debug.Log("Sonify: " + infoString);
-        float freq = float.Parse(infoString)*100;
+        float freq = float.Parse(infoString) * 1000;
         // 那个verb是可以加上reverb的效果，就更有空间感的声音
         //  verb => dac;
         //  0.1 => verb.mix;
