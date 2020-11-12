cbbc73a759e063018bec6f5d5c76c1eb7679dfa2
Zhuoyue Lyu <Zhuoyue.Lyu@ibm.com>
2020-11-11 23:44:15 -0500

    update

---
+++ Assets/Scripts/ForceGraph/Node.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ForceGraph {
+
+    public class Node : MonoBehaviour {
+
+        public string id;
+        // public TextMesh nodeText;
+
+        void Update () {
+            //node text always facing camera
+            // nodeText.transform.LookAt (Camera.main.transform);
+        }
+    }
+
+}
